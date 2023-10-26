192b4360e82262909f08d686afd7845d54b12d74
Ali-Shahmansouri <sctshahali@gmail.com>
2023-10-27 01:53:17 +0330

    Add missing fields to the user schema

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -1,8 +1,15 @@
 export interface IUser {
+  bio?: string;
+  college?: string;
+  is_active?: boolean;
+  is_male?: boolean;
+  is_onboarded?: boolean;
+  rank?: number;
+  last_login?: string;
   user_id?: number;
   name?: string;
   username?: string;
-  imageURL?: string;
+  profile_image?: string;
   token?: string;
   email?: string;
   is_email_verified?: boolean;
