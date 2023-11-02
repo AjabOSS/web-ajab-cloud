a4bc4e06f631e5e84f2b76cd694698963110b9c0
Ali-Shahmansouri <sctshahali@gmail.com>
2023-11-02 15:50:30 +0330

    Add server src to whitelist

--- env.ts
+++ env.ts
@@ -2,10 +2,12 @@ import { z } from "zod";
 
 const envSchema = z.object({
   NEXT_PUBLIC_BASE_URL: z.string().nonempty(),
+  NEXT_PUBLIC_FILE_BASE_URL: z.string().nonempty(),
 });
 
 const env = envSchema.parse({
   NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  NEXT_PUBLIC_FILE_BASE_URL: process.env.NEXT_PUBLIC_FILE_BASE_URL,
 });
 
 export default env;
